567c61bf7d7abd50fe18b7a7cf245126f947756a
Katie Dixon <kpd19@case.edu>
2026-08-31 12:10:52 -0500

    Adding data files and updated Case 1 script

--- analyze/Case1.R
+++ analyze/Case1.R
@@ -12,9 +12,9 @@ case1_ics <- read.table("data/ICgrid/ICgrid_case1_alpha2theta20rF2p5to8p5in25.tx
 
 case1_kde_g2.5_1 <- read_csv("data/kde/tau_case1_0.0_0.0_g2.5_1k.csv")
 
-case1_sim <- read_csv("data/simulations/kde_case1_0.25_2.5to8.5in25_2.csv")
-case1_time <- read_csv("data/simulations/kde_case1_0.25_2.5to8.5in25_time_2.csv")
-case1_gamma <- read_csv("data/simulations/kde_case1_0.25_2.5to8.5in25_gamma_2.csv")
+case1_sim <- read_csv("data/simulationskde_case1_sim_0.0_N2.csv")
+case1_time <- read_csv("data/simulations/kde_case1_sim_0.0_t.csv")
+case1_gamma <- read_csv("data/simulationskde_case1_sim_0.0_d.csv")
 
 colnames(case1_sim) <- c("Competitor","Facilitator")
 case1_sim$time <- case1_time$Column1
@@ -28,6 +28,12 @@ rC = 1.0
 rF = 2.2
 F_eq = (theta*(1- alpha) - 1 + sqrt((1 + theta*(alpha - 1))^2 - 4*theta*(alpha - 1 + dF/rF - alpha*dC/rC)))/(2*theta)
 C_eq = 1 - dC/(rC*(1 + theta*F_eq))
+
+#####################
+#
+# Calculating r-tipping risk
+#
+#####################
 
 case1_kde <- clean_merge(case1_kde_g2.5_1, case1_ics)
 
@@ -57,28 +63,19 @@ dev.off()
 
 calc_ptip(case1_kde_avg)
 
-
-pdf("figures/case1_10k_sim.pdf",height = 6, width = 15)
-case1_sim %>% filter(time >= 250*10000, time <= 750*10000) %>% 
-  ggplot() + aes(x = Competitor, y = Facilitator) + geom_path() + theme_classic()
-dev.off()
-
-comp_col <- '#2596be'
-fac_col <- "#e59f02"
-
-case1_sim_small <- case1_sim %>% filter(time >= 475*10000, time <= 610*10000)
-
-case1_sim_small <- case1_sim_small %>% arrange(time)
-
-case1_sim_small_subsamp = case1_sim_small[seq(1, nrow(case1_sim_small), 20),]
+#####################
+#
+# Plotting simulation
+#
+#####################
 
 comp_col <- '#2596be'
 fac_col <- "#e59f02"
 
 change_df <- data.frame(x1 = 510, x2 = 535, y1 = -0.025, y2 = 0)
 
-pdf("figures/kde/case1_ts_10k_coex2.pdf", height = 6, width = 10)
-plt1 <- case1_sim_small_subsamp %>% ggplot() + geom_line(aes(x = time/10000, y = Competitor, color = "Competitor")) + 
+pdf("figures/kde/case1_ts_10k_coex2.pdf", height = 6, width = 12)
+plt1 <- case1_sim %>% ggplot() + geom_line(aes(x = time/10000, y = Competitor, color = "Competitor")) + 
   geom_line(aes(x = time/10000, y = Facilitator, color = "Facilitator")) + 
   scale_color_manual("", values = c("Facilitator" = fac_col, "Competitor" = comp_col)) + 
   theme_bw(base_size = 15) + 
@@ -89,7 +86,7 @@ plt1 <- case1_sim_small_subsamp %>% ggplot() + geom_line(aes(x = time/10000, y =
   geom_rect(data = change_df, aes(xmin = x1, xmax = x2, ymin = y1, ymax = y2), 
             fill = "grey55", alpha = 0.4, color = "grey55")
 
-plt2 <- case1_sim_small_subsamp %>% filter(time >= 480*10000, time <= 600*10000) %>% 
+plt2 <- case1_sim %>% filter(time >= 480*10000, time <= 600*10000) %>% 
   ggplot() + aes(x = Competitor, y = Facilitator, color = gamma) +
   geom_path() + theme_bw(base_size = 15) + 
   scale_color_scico("d", palette = "roma", limits = c(2.5, 8.5)) + 
